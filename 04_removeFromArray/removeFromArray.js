46502f6cded07723feb379fc1524809fd613e7da
OmarCompiled <179285620+OmarCompiled@users.noreply.github.com>
2025-02-08 07:13:03 +0200

    Done up to 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
+    const new_args = [...args];
+    let new_array = [];
+    array.forEach(element => {
+        if(!new_args.includes(element)) {
+            new_array.push(element);
+        }    
+    });
+    return new_array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+// Done!
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(minLimit, maxLimit) {
+    let sum = 0; // very important!
+    let notNums = (typeof(minLimit) != "number" || typeof(maxLimit) != "number");            // I chose to put each constraint into a bool
+    let isDecimal = (minLimit.toString().includes('.') || maxLimit.toString().includes('.'));// just for readability sake.
+    let isNegative = (minLimit < 0 || maxLimit < 0);                                            
+    if(notNums || isDecimal || isNegative) {
+        return "ERROR";
+    } else {
+        let compare = (a, b) => {a-b;};
+        const Limits = [minLimit, maxLimit];
+        Limits.sort(compare());
+        for(let i = Limits[0]; i <= Limits[1]; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// Done!
