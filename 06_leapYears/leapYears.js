4f1a764207e2550500f1c41504d1ec01c06d7cb1
OmarCompiled <179285620+OmarCompiled@users.noreply.github.com>
2025-02-08 07:21:29 +0200

    Done with 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let leap = (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+    if(leap) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// Done!
