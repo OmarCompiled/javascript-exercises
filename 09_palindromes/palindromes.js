b05fac7c1d47d85194ba61b36fc90e215b7475d0
OmarCompiled <179285620+OmarCompiled@users.noreply.github.com>
2025-03-09 14:20:52 +0200

    Nothing to say

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let reverseString = '';
+    string = string.replaceAll(/(!|@|#|,|%|&| |[0-9])+/g, '').toLowerCase();
+    for(let i = string.length - 1; i >= 0; i--) {
+        reverseString += string.at(i);
+    }
+    if(string === reverseString) {
+        return true;
+    }
+    
+    return false;
 };
 
 // Do not edit below this line
