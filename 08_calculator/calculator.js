94894d50bc92f3ed0dfff231eadbcc2e24b012a8
OmarCompiled <179285620+OmarCompiled@users.noreply.github.com>
2025-03-09 12:07:59 +0200

    08 : calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(a, b) {
+  let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+  args.map(arg => Number(arg));
+  args.sort((a,b) => a > b);
+  let num = args[0];
+  for(let i = 1; i < args.length; i++) {
+    num -= args[i];
+  }
+  return num;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	if(array.length === 0) {
+    return sum;
+  } else {
+    array.forEach(num => sum += num);
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let prod = 1;
+  array.forEach(num => prod *= num);
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let prod = 1;
+  for(let i = 0; i < b; i++) {
+    prod *= a;
+  }
+	return prod;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0) {
+    return 1;
+  } else {
+    return factorial(num - 1) * num;
+  }
 };
 
 // Do not edit below this line
@@ -31,3 +54,6 @@ module.exports = {
   power,
   factorial
 };
+
+
+// done
