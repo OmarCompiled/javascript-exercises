f6224d771101302ddd147e22337111a02e566e85
OmarCompiled <179285620+OmarCompiled@users.noreply.github.com>
2025-03-10 06:07:16 +0200

    DOne

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index = Number(index);
+    if(index === 0) {
+        return 0;
+    } else if(index === 1 | index === 2) {
+        return 1;
+    } else if(index > 2){
+        return fibonacci(index - 1) + fibonacci(index - 2);
+    }
 
+    return "OOPS";
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+//done
