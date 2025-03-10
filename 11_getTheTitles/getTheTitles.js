be994cf756bd8af2e000d0112e003110d79411ae
OmarCompiled <179285620+OmarCompiled@users.noreply.github.com>
2025-03-10 06:30:32 +0200

    Done with 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(booksArray) {
+    const titles = Array.from(booksArray, book => book.title);
+    //alt // return booksArray.map(book => book.title);
 
+    return titles;
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
+//done
